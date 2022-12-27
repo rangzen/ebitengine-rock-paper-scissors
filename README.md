15e7921ffaabb2a5dfae2306d8e9fd9cf43ddafa
Cédric L'homme <public@l-homme.com>
2022-12-27 00:47:33 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Ebiten Rock Paper Scissors
+
+This is a simple rock paper scissors game written in Go using the [Ebitengine](https://ebitengine.org/) game library.
+
+I wanted my own version of https://9gag.com/gag/ap92GVn.
+
+## Running
+
+To run the game, you need to have Go installed. Then, run the following command:
+
+```bash
+go run main.go
+```
+
+## Building
+
+To build the game, run the following command:
+
+```bash
+go build
+```
+
+## Resources
+
+* <a target="_blank" href="https://icons8.com/icon/9FSQ5judlnAN/rock">Rock</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+* <a target="_blank" href="https://icons8.com/icon/jDDj4ExfgPZV/page-facing-up">Page Facing Up</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+* <a target="_blank" href="https://icons8.com/icon/A7egVNynrr0h/scissors">Scissors</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"github.com/hajimehoshi/ebiten/v2"
+	"image"
+	_ "image/png"
+	"log"
+	"math/rand"
+)
+
+// GUI
+const (
+	screenWidth  = 300
+	screenHeight = 600
+	objWidth     = 48 / 2
+	objHeight    = 48 / 2
+	nbObjects    = 50
+)
+
+// States
+const (
+	rock     = iota
+	paper    = iota
+	scissors = iota
+)
+
+//go:embed resources/icons8-rock-48.png
+var rockPng []byte
+var rockImg *ebiten.Image
+
+//go:embed resources/icons8-page-facing-up-48.png
+var paperPng []byte
+var paperImg *ebiten.Image
+
+//go:embed resources/icons8-scissors-48.png
+var scissorsPng []byte
+var scissorsImg *ebiten.Image
+
+// init loads all the resources
+func init() {
+	rockDecoded, _, err := image.Decode(bytes.NewReader(rockPng))
+	if err != nil {
+		log.Fatal("decoding rock:", err)
+	}
+	rockImg = ebiten.NewImageFromImage(rockDecoded)
+
+	paperDecoded, _, err := image.Decode(bytes.NewReader(paperPng))
+	if err != nil {
+		log.Fatal("decoding paper:", err)
+	}
+	paperImg = ebiten.NewImageFromImage(paperDecoded)
+
+	scissorsDecoded, _, err := image.Decode(bytes.NewReader(scissorsPng))
+	if err != nil {
+		log.Fatal("decoding scissors:", err)
+	}
+	scissorsImg = ebiten.NewImageFromImage(scissorsDecoded)
+}
+
+type Obj struct {
+	r    int // resource: rock, paper, scissors
+	x, y int
+}
+
+type Game struct {
+	o [nbObjects]Obj
+}
+
+func (g *Game) Update() error {
+	for i := 0; i < nbObjects; i++ {
+		g.o[i].x += rand.Intn(3) - 1
+		g.o[i].y += rand.Intn(3) - 1
+	}
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	// ebitenutil.DebugPrint(screen, "Rock Paper Scissors")
+	for _, o := range g.o {
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(float64(o.x-objWidth), float64(o.y-objHeight))
+		switch o.r {
+		case rock:
+			screen.DrawImage(rockImg, op)
+		case paper:
+			screen.DrawImage(paperImg, op)
+		case scissors:
+			screen.DrawImage(scissorsImg, op)
+		}
+	}
+}
+
+func (g *Game) Layout(_, _ int) (int, int) {
+	return screenWidth * 2, screenHeight * 2
+}
+
+func (g *Game) Init() {
+	for i := 0; i < nbObjects; i++ {
+		g.o[i] = Obj{
+			r: rand.Intn(3),
+			x: rand.Intn(screenWidth * 2),
+			y: rand.Intn(screenHeight * 2),
+		}
+	}
+}
+
+func main() {
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Rock Paper Scissors")
+	game := &Game{}
+	game.Init()
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
